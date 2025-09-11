523a40e2af63a9503949898b03f11466b224594d
Vladislav-EG <vlad.egipko@mail.ru>
2025-09-11 16:45:49 +0300

    https://github.com/starikcetin/Eflatun.SceneReference installed

--- Assets/Scripts/Bootstrap/BootstrapMono/TestServiceMono.cs
+++ Assets/Scripts/Bootstrap/BootstrapMono/TestServiceMono.cs
@@ -1,8 +1,16 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using Eflatun.SceneReference;
 
 public class TestServiceMono : MonoBehaviour, IService
 {
+	[SerializeField] private SceneReference mySceneReference;
+
+	private void Awake()
+	{
+		Debug.Log(mySceneReference.Name);
+	}
+
 	public async Task InitializeAsync()
 	{
 		Debug.Log("TestServiceMono init started...");
